9b737e4984a9c652569dc8e2c547e9efceac5f91
subodh10000 <145152037+subodh10000@users.noreply.github.com>
2025-03-07 05:37:51 +0000

    Deploying to gh-pages from @ subodh10000/subodh10000.github.io@e38e9dec0e65050ec8fd706ab714ca28ec82ae76 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-resume",
           title: "Resume",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My updated resume March 2025.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
