f1a3e23165d5761fb3027abc3fd0ae9be0d723be
subodh10000 <145152037+subodh10000@users.noreply.github.com>
2025-03-07 03:27:57 +0000

    Deploying to gh-pages from @ subodh10000/subodh10000.github.io@80130e656928bb35ae60765ee542791adcfd8fbb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-resume",
+          title: "Resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
