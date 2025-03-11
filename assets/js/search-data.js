57a0112d1184e8c3be27b1a874c3b2186031fcb6
subodh10000 <145152037+subodh10000@users.noreply.github.com>
2025-03-11 02:03:03 +0000

    Deploying to gh-pages from @ subodh10000/subodh10000.github.io@891ad010c6fee487286b123e63af927e501aa81d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,9 +405,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-lofi-steampunk",
+          title: 'LoFi Steampunk',
+          description: "learning chords made easy",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
